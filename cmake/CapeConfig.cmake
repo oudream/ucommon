e37423124d46b8afc7a8a910b04fd5c8d284ebc3
David Sugar <dyfet@gnutelephony.org>
2015-02-24 07:59:09 -0500

    more cmake improvements

---
+++ cmake/CapeConfig.cmake
@@ -0,0 +1,126 @@
+#.rst:
+# CapeConfig
+# ---------------
+
+#=============================================================================
+# Copyright (C) 2015 David Sugar, Tycho Softworks.
+#
+# This file is free software; as a special exception the author gives
+# unlimited permission to copy and/or distribute it, with or without
+# modifications, as long as this notice is preserved.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#=============================================================================
+
+include(GNUInstallDirs)
+include(CheckFunctionExists)
+include(CheckIncludeFiles)
+
+macro(check_functions)
+    foreach(arg ${ARGN})
+        string(TOUPPER "${arg}" _fn)
+        set(_fn "HAVE_${_fn}")
+        check_function_exists(${arg} ${_fn})
+    endforeach()
+endmacro()
+
+macro(check_headers)
+    foreach(arg ${ARGN})
+        string(TOUPPER "${arg}" _hdr)
+        string(REGEX REPLACE "/" "_" _hdr ${_hdr})
+        string(REGEX REPLACE "[.]" "_" _hdr ${_hdr})
+        set(_hdr "HAVE_${_hdr}")
+        check_include_files(${arg} ${_hdr})
+    endforeach()
+endmacro()
+
+macro(target_setuid_properties)
+    if(CMAKE_COMPILER_IS_GNUCXX AND NOT CMAKE_COMPILE_SETUID_FLAGS)
+	    set(CMAKE_COMPILE_SETUID_FLAGS "-O2 -fPIE -fstack-protector -D_FORTIFY_SOURCE=2 --param ssp-buffer-size=4 -pie") 
+        set(CMAKE_LINK_SETUID_FLAGS "-pie -z relro -z now")
+    endif()
+    foreach(arg ${ARGN})
+        set_target_properties(${arg} PROPERTIES COMPILE_FLAGS "${CMAKE_COMPILE_SETUID_FLAGS}" LINK_FLAGS "${CMAKE_LINK_SETUID_FLAGS}" POSITION_INDEPENDENT_CODE TRUE )
+    endforeach()
+endmacro()
+
+function(add_library_version _LIBRARY)
+    set(_VERSION "${ARGN}")
+    set(_SOVERSION "-")
+    if(UNIX AND "${_VERSION}" STREQUAL "" AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/cmake-abi.sh")
+        execute_process(WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} COMMAND "./cmake-abi.sh" OUTPUT_VARIABLE _VERSION)
+    endif()
+	if(NOT UNIX OR "${_VERSION}" STREQUAL "")
+		set(_VERSION} "${VERSION}")
+	endif()
+	STRING(REGEX REPLACE "[.].*$" "" _SOVERSION ${_VERSION})
+	if(ABI_MAJORONLY)
+		set(_VERSION ${_SOVERSION})
+	endif()
+    set_target_properties(${_LIBRARY} PROPERTIES VERSION ${_VERSION} SOVERSION ${_SOVERSION})
+endfunction()
+
+function(create_specfile)
+    if(ARGN)
+        foreach(arg ${ARGN})
+            configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${arg}.spec.cmake ${CMAKE_CURRENT_SOURCE_DIR}/${arg}.spec @ONLY)
+        endforeach()
+    else()
+        configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.spec.cmake ${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.spec @ONLY)
+    endif()
+endfunction()
+
+function(create_rcfiles _OUTPUT)
+    set(RC_VERSION ${VERSION})
+	string(REGEX REPLACE "[.]" "," RC_VERSION ${VERSION})
+	set(RC_VERSION "${RC_VERSION},0")
+    set(RC_FILES)
+
+    if(ARGN)
+        foreach(arg ${ARGN})
+            configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${arg}.rc.cmake ${CMAKE_CURRENT_SOURCE_DIR}/${arg}.rc)
+            set(RC_FILES ${RC_FILES} ${arg}.rc)
+        endforeach()
+    else()
+        configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.rc.cmake ${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.rc)
+        set(RC_FILES ${PROJECT_NAME}.rc)
+    endif()
+    if(WIN32)
+        set(${_OUTPUT} ${RC_FILES} PARENT_SCOPE)
+    else()
+        set(${_OUTPUT} PARENT_SCOPE)
+    endif()
+endfunction()
+
+function(create_pcfiles _OUTPUT)
+    set(PC_FILES)
+    if(${CMAKE_INSTALL_LIBDIR})
+        set(_pkgfiles "${CMAKE_INSTALL_LIBDIR}/pkgconfig")
+    else()
+        set(_pkgfiles "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig")
+    endif()
+    if(ARGN)
+        foreach(arg ${ARGN})
+            configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${arg}.pc.cmake ${CMAKE_CURRENT_BINARY_DIR}/${arg}.pc)
+            set(PC_FILES ${PC_FILES} ${CMAKE_CURRENT_BINARY_DIR}/${arg}.pc)
+        endforeach()
+    else()
+        configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}.pc.cmake ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc)
+        set(PC_FILES ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc)
+    endif()
+    set(${_OUTPUT} ${PC_FILES} PARENT_SCOPE)
+endfunction()
+
+function(create_headers)
+    if(ARGN)
+        foreach(arg ${ARGN})
+            configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${arg}.cmake ${CMAKE_CURRENT_BINARY_DIR}/${arg})
+        endforeach()
+    else()
+        configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}-config.h.cmake ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}-config.h)
+    endif()
+    include_directories(${CMAKE_CURRENT_BINARY_DIR})
+endfunction()
+
---
+++ cmake/CapeMakeTargets.cmake
@@ -0,0 +1,112 @@
+#.rst:
+# CapeMakeTargets
+# ---------------
+
+#=============================================================================
+# Copyright (C) 2015 David Sugar, Tycho Softworks.
+#
+# This file is free software; as a special exception the author gives
+# unlimited permission to copy and/or distribute it, with or without
+# modifications, as long as this notice is preserved.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#=============================================================================
+
+macro(add_make_dist_target _TARGET _VERSION)
+    option(BUILD_DISTFILE "Set to ON to create tarball distfile" OFF)
+
+    if(BUILD_DISTFILE OR CMAKE_GENERATOR MATCHES "Unix Makefiles")
+        add_custom_target(cleandist
+            WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+            COMMAND rm -f "${CMAKE_CURRENT_BINARY_DIR}/${_TARGET}[-_]*.gz"
+            COMMAND rm -f "${CMAKE_CURRENT_BINARY_DIR}/${_TARGET}_*.dsc"
+            COMMAND rm -f "${CMAKE_CURRENT_BINARY_DIR}/${_TARGET}-*.rpm"
+            COMMAND rm -f "${CMAKE_CURRENT_BINARY_DIR}/${_TARGET}[-_]*.deb"
+            COMMAND rm -f "${CMAKE_CURRENT_BINARY_DIR}/*${_TARGET}*[-_]*.deb"
+            COMMAND rm -f "${CMAKE_CURRENT_BINARY_DIR}/${_TARGET}-*.zip"
+            COMMAND rm -f "${CMAKE_CURRENT_BINARY_DIR}/*${_TARGET}*.changes"
+        )
+
+        add_custom_target(dist
+            DEPENDS cleandist
+            WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+            COMMAND git archive --format tar.gz --output="${CMAKE_CURRENT_BINARY_DIR}/${_TARGET}-${_VERSION}.tar.gz" --prefix="${_TARGET}-${_VERSION}/" HEAD
+            COMMAND git archive --format zip --output="${CMAKE_CURRENT_BINARY_DIR}/${_TARGET}-${_VERSION}.zip" --prefix="${_TATGET}-${_VERSION}/" HEAD
+        )
+    endif()
+endmacro(add_make_dist_target)
+
+macro(add_make_srpm_target _TARGET)
+    if(UNIX AND CMAKE_GENERATOR MATCHES "Unix Makefiles")
+        add_custom_target(srpm
+            WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+            COMMAND rm -f *.rpm
+            COMMAND rpmbuild  -bs --nodeps --define "_sourcedir ." --define "_srcrpmdir ." --sign ${_TARGET}.spec
+        )
+    endif()
+endmacro()
+
+macro(add_make_uninstall_target)
+    if(CMAKE_GENERATOR MATCHES "Unix Makefiles")
+        add_custom_target(uninstall
+            WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+            COMMAND xargs <install_manifest.txt
+        )
+    endif()
+endmacro()
+
+
+macro(add_make_deb_target _TARGET _VERSION)
+    if(LINUX AND CMAKE_GENERATOR MATCHES "Unix Makefiles")
+        if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/packaging/debian/")
+            add_custom_target(deb
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                COMMAND rm -f *.deb *.debian.tar.gz *.dsc *.changes
+                COMMAND cape-source --sign ${_TARGET}-${_VERSION}.tar.gz packaging
+            )
+        endif()
+    endif()
+endmacro()
+
+macro(add_cape_make_targets _TARGET _VERSION)
+    add_make_dist_target(${_TARGET} ${_VERSION})
+    add_make_deb_target(${_TARGET} ${_VERSION})
+    add_make_srpm_target(${_TARGET})
+    add_make_uninstall_target()
+endmacro()
+
+macro(add_cape_docs_target _DOXYFILE)
+    option(BUILD_DOCS "Set to ON to create doxygen docs" OFF)
+    if(BUILD_DOCS OR CMAKE_GENERATOR MATCHES "Unix Makefiles")
+        find_package(Doxygen)
+    endif()
+    if(DOXYGEN_FOUND)
+        configure_file(${CMAKE_CURRENT_SOURCE_DIR}/${_DOXYFILE}.cmake
+            ${CMAKE_CURRENT_BINARY_DIR}/${_DOXYFILE} @ONLY )
+        add_custom_target(doc
+            ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/${_DOXYFILE}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            COMMENT "Generating API documentation with Doxygen" VERBATIM
+        )
+        if(WIN32)
+            find_package(HTMLHelp)
+            if(HTML_HELP_COMPILER AND EXISTS "doc/")
+                set(TMP "${CMAKE_CURRENT_BINARY_DIR}\\doc\\html\\index.hhp")
+                string(REGEX REPLACE "[/]" "\\\\" HHP_FILE ${TMP} )
+                add_custom_target(winhelp ${HTML_HELP_COMPILER} ${HHP_FILE})
+                add_dependencies(winhelp doc)
+            endif()
+        endif()
+    endif()
+endmacro()
+
+macro(add_update_cmake_target)
+    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/cmake/" AND CMAKE_GENERATOR MATCHES "Unix Makefiles")
+        add_custom_target(update-cmake
+            WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+            COMMAND ./update-cmake.sh "${CMAKE_ROOT}/Modules" ${ARGN}
+        )
+    endif()
+endmacro()
