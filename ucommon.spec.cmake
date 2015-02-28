d44a882c40a93ba6d1b6d5f7d306329699b39e6d
David Sugar <dyfet@gnutelephony.org>
2015-02-28 08:40:21 -0500

    fix specs

--- ucommon.spec.cmake
+++ ucommon.spec.cmake
@@ -119,7 +119,7 @@ make install DESTDIR=%{buildroot}
 %{_bindir}/ucommon-config
 %{_bindir}/commoncpp-config
 # %{_mandir}/man1/ucommon-config.*
-%{_datadir}/cmake/GNUTelephony/Cape*.cmake
+%{_datadir}/libucommon/Cape*.cmake
 
 %files doc
 %defattr(-,root,root,-)
